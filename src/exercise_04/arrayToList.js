8c9f7503ba821c526a4acfbfa7757092148dacb9
ClarkTheSup <lknkoqq123@163.com>
2020-07-22 12:02:49 +0800

    submit exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,21 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null && array == undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  function Node (value) {
+    this.value = value;
+    this.next = null;
+  }
+  const list = new Node(null);
+  let p = list;
+  for (let i = 0; i < array.length; i++) {
+    p.next = new Node(array[i]);
+    p = p.next;
+  }
+  return list.next;
 }
