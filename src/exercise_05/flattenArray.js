c7db9aa08775f68dc1a3f8ecf61fa47c452fcea6
ClarkTheSup <lknkoqq123@163.com>
2020-07-22 12:19:47 +0800

    submit exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,21 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null && array == undefined) {
+    throw new Error('Flatten undefined or null array');
+  }
+  if (array.length === 0) {
+    return [];
+  }
+  const new_arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] instanceof Array) {
+      for (let j = 0; j < array[i].length; j++) {
+        new_arr.push(array[i][j]);
+      }
+    } else {
+      new_arr.push(array[i]);
+    }
+  }
+  return new_arr;
 }
