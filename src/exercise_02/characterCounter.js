7629774356168e26ce00f490290233c95a371f12
ClarkTheSup <lknkoqq123@163.com>
2020-07-22 11:31:56 +0800

    submit exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,15 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string == null || string === 'undefined') {
+    return 0;
+  }
+  if (prediction == null || prediction === 'undefined') {
+    return string.length;
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (prediction(string[i])) {
+      return i;
+    }
+  }
 }
